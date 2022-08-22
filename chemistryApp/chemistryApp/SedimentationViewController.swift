169185b5b28378421e80f0f801cdcd0f9329403a
Gabriel Baptista Lopes <gabriel_b_lopes@hotmail.com>
2022-08-22 12:57:11 -0300

    Criada as Outlets da sedimentationViewController

--- chemistryApp/chemistryApp/SedimentationViewController.swift
+++ chemistryApp/chemistryApp/SedimentationViewController.swift
@@ -8,14 +8,23 @@
 import UIKit
 
 class SedimentationViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var solidDensityTextField: UITextField!
+    @IBOutlet weak var fluidDensityTextField: UITextField!
+    @IBOutlet weak var dynamicViscosityTextField: UITextField!
+    @IBOutlet weak var solidDiameterTextField: UITextField!
+    @IBOutlet weak var lawTypeLabel: UILabel!
+    @IBOutlet weak var freeFallCoeficientLabel: UILabel!
+    @IBOutlet weak var reynoldsLabel: UILabel!
+    @IBOutlet weak var terminalVelocityLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-   
 
 }
