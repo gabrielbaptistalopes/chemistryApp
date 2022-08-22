25e2022923de973abe3cd8067c6fd18d4796f0ff
Gabriel Baptista Lopes <103600992+gabrielbaptistalopes@users.noreply.github.com>
2022-08-22 12:29:29 -0300

    Add files via upload

---
+++ chemistryApp/chemistryApp/ConvertSedimentationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ConvertSedimentationViewController.swift
+//  chemistryApp
+//
+//  Created by Gabriel Lopes on 19/08/22.
+//
+
+import UIKit
+
+class ConvertSedimentationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
---
+++ chemistryApp/chemistryApp/SedimentationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SedimentationViewController.swift
+//  chemistryApp
+//
+//  Created by Gabriel Lopes on 18/08/22.
+//
+
+import UIKit
+
+class SedimentationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
