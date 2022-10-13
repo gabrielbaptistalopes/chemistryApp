082ca9f9c825fb45303313b54ac63fd9e46977e2
Gabriel Baptista Lopes <gabriel_b_lopes@hotmail.com>
2022-10-13 10:25:18 -0300

    Feitas as Outlets da convertedSedimentationViewController

--- chemistryApp/chemistryApp/ConvertSedimentationViewController.swift
+++ chemistryApp/chemistryApp/ConvertSedimentationViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 class ConvertSedimentationViewController: UIViewController {
+    
+    @IBOutlet weak var densityToConvert: UITextField!
+    @IBOutlet weak var convertedValueDensityLabel: UILabel!
+    @IBOutlet weak var dynamicViscosityToConvert: UITextField!
+    @IBOutlet weak var convertedValueViscosityLabel: UILabel!
+    @IBOutlet weak var diameterToConvert: UITextField!
+    @IBOutlet weak var convertedDiameterLabel: UILabel!
+    @IBOutlet weak var densityConverterUIButton: UIButton!
+    @IBOutlet weak var dynamicViscosityConverterUIButton:
+    UIButton!
+    @IBOutlet weak var diameterConverterUIButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +27,7 @@ class ConvertSedimentationViewController: UIViewController {
         
     }
     
-
+    
    
 
 }
